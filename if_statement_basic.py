0ac7530930862ad7593fed2688b77d10efcac949
LorenzoVald <>
2025-02-19 12:55:07 +0800

    Last Sync: 2025-02-19 04:55:07 (Mobile)

--- if_statement_basic.py
+++ if_statement_basic.py
@@ -1,4 +1,4 @@
-print('It\'s a hot day!')
+print("It's a hot day!")  # Fixed string issue
 temperature = 35  # Or any other number
 if temperature > 30:
     print("It's a hot day!")  # Indented 4 spaces
